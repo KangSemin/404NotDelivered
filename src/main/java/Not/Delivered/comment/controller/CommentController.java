aad6e5c36b658e62dc00a40263b9f9dbf5be93e0
heehyun0221 <psr4580@gmail.com>
2025-01-08 17:50:28 +0900

    🚑 fix : 테스트용 코드 삭제

--- src/main/java/Not/Delivered/comment/controller/CommentController.java
+++ src/main/java/Not/Delivered/comment/controller/CommentController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,10 +23,9 @@ public class CommentController {
   private final CommentService commentService;
 
   @PostMapping
-  public ResponseEntity<ApiResponse<CommentDto>> createComment(/*@RequestAttribute Long userId,*/
+  public ResponseEntity<ApiResponse<CommentDto>> createComment(@RequestAttribute Long userId,
       @RequestBody CommentCreateRequestDto requestDto,
       @PathVariable("reviewId") Long reviewId) {
-    Long userId = 1L;
     CommentDto comment = commentService.createComment(userId, reviewId, requestDto);
     ApiResponse<CommentDto> apiResponse = ApiResponse.success(HttpStatus.CREATED, "코멘트 생성 성공",
         comment);
@@ -36,9 +36,8 @@ public class CommentController {
 
   @DeleteMapping("{commentId}")
   public ResponseEntity<ApiResponse<String>> deleteComment(@PathVariable Long commentId,
-      @PathVariable("reviewId") Long reviewId/*,
-      @RequestAttribute Long userId*/) {
-    Long userId = 1L;
+      @PathVariable("reviewId") Long reviewId,
+      @RequestAttribute Long userId) {
     commentService.deleteComment(userId,reviewId,commentId);
     ApiResponse<String> apiResponse = ApiResponse.success(HttpStatus.OK, "코멘트 삭제 성공", null);
     return new ResponseEntity<>(apiResponse, HttpStatus.OK);
--- src/main/java/Not/Delivered/review/controller/ReviewController.java
+++ src/main/java/Not/Delivered/review/controller/ReviewController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,9 +23,8 @@ public class ReviewController {
   private final ReviewService reviewService;
 
   @PostMapping
-  public ResponseEntity<ApiResponse<ReviewDto>> createReview(/*@RequestAttribute Long userId,*/
+  public ResponseEntity<ApiResponse<ReviewDto>> createReview(@RequestAttribute Long userId,
       @RequestBody ReviewCreateRequestDto requestDto) {
-    Long userId = 1L;
     ReviewDto review = reviewService.createReview(userId, requestDto);
     ApiResponse<ReviewDto> apiResponse = ApiResponse.success(HttpStatus.CREATED, "리뷰 생성 성공",
         review);
@@ -34,9 +34,8 @@ public class ReviewController {
   //TODO 리뷰 수정 기능
 
   @DeleteMapping("/{reviewId}")
-  public ResponseEntity<ApiResponse<String>> deleteReview(@PathVariable Long reviewId/*,
-      @RequestAttribute Long userId*/) {
-    Long userId = 1L;
+  public ResponseEntity<ApiResponse<String>> deleteReview(@PathVariable Long reviewId,
+      @RequestAttribute Long userId) {
     reviewService.deleteReview(userId, reviewId);
     ApiResponse<String> apiResponse = ApiResponse.success(HttpStatus.OK, "리뷰 삭제 성공", null);
     return new ResponseEntity<>(apiResponse, HttpStatus.OK);
