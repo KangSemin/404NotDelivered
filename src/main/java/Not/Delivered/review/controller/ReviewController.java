670b5a18f7d8dbfe65681b086a75d735abd8ae7c
heehyun0221 <psr4580@gmail.com>
2025-01-08 14:59:26 +0900

    ✨ feat : 리뷰 삭제 기능 추가

--- src/main/java/Not/Delivered/review/controller/ReviewController.java
+++ src/main/java/Not/Delivered/review/controller/ReviewController.java
@@ -7,6 +7,8 @@ import Not.Delivered.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,14 +23,20 @@ public class ReviewController {
   private final ReviewService reviewService;
 
   @PostMapping
-  public ResponseEntity<ApiResponse<ReviewDto>> createReview(
-      @RequestAttribute Long userId,
-      @RequestBody ReviewCreateRequestDto requestDto
-  ) {
-      ReviewDto review = reviewService.createReview(userId,requestDto);
-      ApiResponse<ReviewDto> apiResponse = ApiResponse.success(HttpStatus.CREATED, "리뷰 생성 성공",review);
-    return new ResponseEntity<>(apiResponse,HttpStatus.CREATED);
+  public ResponseEntity<ApiResponse<ReviewDto>> createReview(@RequestAttribute Long userId,
+      @RequestBody ReviewCreateRequestDto requestDto) throws Exception {
+    ReviewDto review = reviewService.createReview(userId, requestDto);
+    ApiResponse<ReviewDto> apiResponse = ApiResponse.success(HttpStatus.CREATED, "리뷰 생성 성공",
+        review);
+    return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
   }
 
+  @DeleteMapping("/{reviewId}")
+  public ResponseEntity<ApiResponse<String>> deleteReview(@PathVariable Long reviewId,
+      @RequestAttribute Long userId) throws Exception {
+    reviewService.deleteReview(userId, reviewId);
+    ApiResponse<String> apiResponse = ApiResponse.success(HttpStatus.OK, "리뷰 삭제 성공", null);
+    return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+  }
 
 }
--- src/main/java/Not/Delivered/review/service/ReviewService.java
+++ src/main/java/Not/Delivered/review/service/ReviewService.java
@@ -1,6 +1,7 @@
 package Not.Delivered.review.service;
 
 import Not.Delivered.purchase.domain.Purchase;
+import Not.Delivered.purchase.domain.PurchaseStatus;
 import Not.Delivered.purchase.repository.PurchaseRepository;
 import Not.Delivered.review.domain.Dto.ReviewCreateRequestDto;
 import Not.Delivered.review.domain.Dto.ReviewDto;
@@ -10,6 +11,7 @@ import Not.Delivered.shop.domain.Shop;
 import Not.Delivered.shop.repository.ShopRepository;
 import Not.Delivered.user.domain.User;
 import Not.Delivered.user.repository.UserRepository;
+import java.nio.file.AccessDeniedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -23,13 +25,20 @@ public class ReviewService {
   private final ShopRepository shopRepository;
 
 
-  public ReviewDto createReview(Long userId, ReviewCreateRequestDto requestDto) {
+  public ReviewDto createReview(Long userId, ReviewCreateRequestDto requestDto)
+      throws AccessDeniedException {
     Purchase purchase = purchaseRepository.findById(requestDto.purchaseId()).orElseThrow(
-        () -> new IllegalArgumentException("Purchase not found with ID:" + requestDto.purchaseId()));
+        () -> new IllegalArgumentException(
+            "Purchase not found with ID:" + requestDto.purchaseId()));
 
     // 주문한유저와 접속한 유저가 동일인이 아닐 경우 예외처리, 엔티티에 위임
     if (!purchase.getPurchaseUser().getUserId().equals(userId)) {
-      //accessDenied Exception
+      throw new AccessDeniedException("해당 주문의 유저만 리뷰를 작성할 수 있습니다.");
+    }
+
+    // 주문 상태가 '배달완료'가 아닐때 예외처리
+    if (purchase.getPurchaseStatus().equals(PurchaseStatus.DELIVERED)) {
+      throw new AccessDeniedException("배달 완료되지 않은 주문은 리뷰를 남길 수 없습니다.");
     }
 
     User user = userRepository.findById(userId)
@@ -45,5 +54,18 @@ public class ReviewService {
     reviewRepository.save(review);
 
     return ReviewDto.convertDto(review);
+  }
+
+  public void deleteReview(Long userId, Long reviewId) throws AccessDeniedException {
+    Review review = reviewRepository.findById(reviewId)
+        .orElseThrow(() -> new IllegalArgumentException("Review not fount with ID:" + reviewId));
+
+    //본인의 리뷰가 아니면 삭제할 수 없음
+    if (!review.getUser().getUserId().equals(userId)) {
+      throw new AccessDeniedException("본인의 리뷰만 삭제할 수 있습니다.");
+    }
+
+    reviewRepository.delete(review);
+
   }
 }
