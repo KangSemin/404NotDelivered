0514071c40ab14c8cbef54895163a294d1409dc7
star-pooh <eunki92@gmail.com>
2025-01-09 16:15:32 +0900

    ✨ feat : Add Menu Create API - 메뉴 생성 API 추가

--- src/main/java/Not/Delivered/menu/dto/MenuCreateRequestDto.java
+++ src/main/java/Not/Delivered/menu/dto/MenuCreateRequestDto.java
@@ -1,4 +1,20 @@
 package Not.Delivered.menu.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public class MenuCreateRequestDto {
+
+  @NotNull(message = "가게 ID는 필수 입력 값입니다.")
+  private final Long shopId;
+
+  @NotBlank(message = "메뉴 이름은 필수 입력 값입니다.")
+  private final String menuName;
+
+  @NotNull(message = "가격은 필수 입력 값입니다.")
+  private final Integer price;
 }
--- src/main/java/Not/Delivered/menu/service/MenuService.java
+++ src/main/java/Not/Delivered/menu/service/MenuService.java
@@ -1,4 +1,38 @@
 package Not.Delivered.menu.service;
 
+import Not.Delivered.menu.domain.Menu;
+import Not.Delivered.menu.dto.MenuCreateRequestDto;
+import Not.Delivered.menu.dto.MenuCreateResponseDto;
+import Not.Delivered.menu.repository.MenuRepository;
+import Not.Delivered.shop.domain.Shop;
+import Not.Delivered.shop.service.ShopService;
+import Not.Delivered.user.domain.UserStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MenuService {
+
+  private final MenuRepository menuRepository;
+  private final ShopService shopService;
+
+  public MenuCreateResponseDto createMenu(
+      Long userId, UserStatus userRole, MenuCreateRequestDto dto) {
+    if (!userRole.equals(UserStatus.OWNER)) {
+      throw new IllegalArgumentException("사장님만 사용할 수 있는 기능입니다.");
+    }
+
+    Shop foundShop = shopService.foundAndValidate(userId, dto.getShopId());
+
+    Menu newMenu =
+        Menu.builder().shop(foundShop).menuName(dto.getMenuName()).price(dto.getPrice()).build();
+
+    Menu savedMenu = menuRepository.save(newMenu);
+
+    return MenuCreateResponseDto.builder()
+        .menuName(savedMenu.getMenuName())
+        .price(savedMenu.getPrice())
+        .build();
+  }
 }
