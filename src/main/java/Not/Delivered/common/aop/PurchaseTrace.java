0e40e92e20b5561e2b41fa22787296e5a74b0c3e
heehyun0221 <psr4580@gmail.com>
2025-01-11 15:26:41 +0900

    🚑 fix

--- src/main/java/Not/Delivered/common/aop/PurchaseTrace.java
+++ src/main/java/Not/Delivered/common/aop/PurchaseTrace.java
@@ -3,6 +3,7 @@ package Not.Delivered.common.aop;
 
 import Not.Delivered.common.dto.ApiResponse;
 import Not.Delivered.purchase.dto.PurchaseDto;
+import Not.Delivered.purchase.dto.PurchaseOfRiderDto;
 import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -49,6 +50,39 @@ public class PurchaseTrace {
     String shopId = ((PurchaseDto)((ApiResponse) ((ResponseEntity) result)
         .getBody()).getData()).getShopId().toString();
     String purchaseStatus = ((PurchaseDto)((ApiResponse) ((ResponseEntity) result)
+        .getBody()).getData()).getPurchaseStatus().toString();
+
+    logMessage.append("[purchase log ").append(LocalDateTime.now()).append("] : requestUser = ")
+        .append(request.getAttribute("userId")).append(", requestURL = ")
+        .append(request.getRequestURI()).append(", purchase ID = ").append(purchaseId)
+        .append(", shop ID = ").append(shopId).append(", purchaseStatus = ").append(purchaseStatus);
+
+    return logMessage.toString();
+  }
+
+  @Around("execution(public * Not.Delivered.purchase.controller.PurchaseRiderController.*(..))")
+  public Object purchaseRiderLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+    ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+    HttpServletRequest request = Objects.requireNonNull(attributes).getRequest();
+
+    if("GET".equalsIgnoreCase(request.getMethod())){
+      return joinPoint.proceed();
+    }
+
+    Object result = joinPoint.proceed();
+    logger.info(getRiderLogMessage(request,result));
+
+    return result;
+  }
+
+  private String getRiderLogMessage(HttpServletRequest request, Object result) {
+    StringBuilder logMessage = new StringBuilder();
+
+    String purchaseId = ((PurchaseOfRiderDto)((ApiResponse) ((ResponseEntity) result)
+        .getBody()).getData()).getPurchaseId().toString();
+    String shopId = ((PurchaseOfRiderDto)((ApiResponse) ((ResponseEntity) result)
+        .getBody()).getData()).getShopId().toString();
+    String purchaseStatus = ((PurchaseOfRiderDto)((ApiResponse) ((ResponseEntity) result)
         .getBody()).getData()).getPurchaseStatus().toString();
 
     logMessage.append("[purchase log ").append(LocalDateTime.now()).append("] : requestUser = ")
