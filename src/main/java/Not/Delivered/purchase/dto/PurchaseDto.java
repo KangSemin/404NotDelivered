069ced316c72be05b75fcef59449563fc11a076c
HEEHYUN0221 <asw03131@naver.com>
2025-01-13 11:47:54 +0900

    Update PurchaseDto.java

--- src/main/java/Not/Delivered/purchase/dto/PurchaseDto.java
+++ src/main/java/Not/Delivered/purchase/dto/PurchaseDto.java
@@ -1,13 +1,13 @@
-package Not.Delivered.purchase.dto;
 
 import Not.Delivered.purchase.domain.Purchase;
 import Not.Delivered.purchase.domain.PurchaseStatus;
+
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 public class PurchaseDto {
-
   private final Long purchaseId;
   private final Long purchaseUserId;
   private final Long deliveringUserId;
@@ -15,7 +15,6 @@ public class PurchaseDto {
   private final Long menuId;
   private final PurchaseStatus purchaseStatus;
   private final boolean isCancelled;
-
   @Builder
   public PurchaseDto(Long purchaseId, Long purchaseUserId, Long deliveringUserId, Long shopId,
       Long menuId, PurchaseStatus purchaseStatus, boolean isCancelled) {
@@ -29,10 +28,14 @@ public class PurchaseDto {
   }
 
   public static PurchaseDto convertToDto(Purchase purchase) {
-    return PurchaseDto.builder().purchaseId(purchase.getPurchaseId())
-        .purchaseUserId(purchase.getPurchaseUser().getUserId()).deliveringUserId(
-            purchase.getDeliveringUser() != null ? purchase.getDeliveringUser().getUserId() : null)
-        .shopId(purchase.getShop().getShopId()).menuId(purchase.getMenu().getMenuId())
-        .purchaseStatus(purchase.getPurchaseStatus()).isCancelled(purchase.isCancelled()).build();
+    return PurchaseDto.builder()
+        .purchaseId(purchase.getPurchaseId())
+        .purchaseUserId(purchase.getPurchaseUser().getUserId())
+        .deliveringUserId(purchase.getDeliveringUser() != null ? purchase.getDeliveringUser().getUserId() : null)
+        .shopId(purchase.getShop().getShopId())
+        .menuId(purchase.getMenu().getMenuId())
+        .purchaseStatus(purchase.getPurchaseStatus())
+        .isCancelled(purchase.isCancelled())
+        .build();
   }
 }
