5949699c2a6ec3842a9af5dedd4f7fb6d102c452
heehyun0221 <psr4580@gmail.com>
2025-01-13 10:12:18 +0900

    🚑 fix  : 탈퇴한 유저가 로그인할 수 있었던 문제 수정

--- src/main/java/Not/Delivered/auth/service/AuthService.java
+++ src/main/java/Not/Delivered/auth/service/AuthService.java
@@ -6,41 +6,40 @@ import Not.Delivered.auth.dto.SignupRequestDto;
 import Not.Delivered.common.config.PasswordEncoder;
 import Not.Delivered.user.domain.User;
 import Not.Delivered.user.repository.UserRepository;
+import Not.Delivered.user.service.TempUserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
-	private final UserRepository userRepository;
-	private final PasswordEncoder passwordEncoder;
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
 
 
-	public User signUpUser(SignupRequestDto request) {
-		String encodedPassword = passwordEncoder.encode(request.getPassword());
+  public User signUpUser(SignupRequestDto request) {
+    String encodedPassword = passwordEncoder.encode(request.getPassword());
 
-		User user = User.builder()
-				.email(request.getEmail())
-				.password(encodedPassword)
-				.userName(request.getUsername())
-				.userStatus(request.getUserStatus())
-				.address(request.getAddress())
-				.build();
+    User user = User.builder().email(request.getEmail()).password(encodedPassword)
+        .userName(request.getUsername()).userStatus(request.getUserStatus())
+        .address(request.getAddress()).build();
 
-		return userRepository.save(user);
-	}
+    return userRepository.save(user);
+  }
 
 
   public User login(LoginRequestDto request) {
-		User user = userRepository.findByEmail(request.getEmail())
-				.orElseThrow(() -> new IllegalArgumentException("등록되지 않은 이메일입니다."));
+    User user = userRepository.findByEmail(request.getEmail())
+        .orElseThrow(() -> new IllegalArgumentException("등록되지 않은 이메일입니다."));
+    if (user.getIsWithdrawal()) {
+      throw new TempUserNotFoundException("이미 탈퇴한 유저입니다.");
+    }
 
-		if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-			throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
-		}
+    if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+      throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+    }
 
-		return user;
+    return user;
   }
 }
